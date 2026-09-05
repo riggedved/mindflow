033e909f4b04b9615e00e186c2762f7dde54e399
Ved Saxena <vedsaxena19458@gmail.com>
2026-09-06 01:11:41 +0530

    updated the CORS

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -12,7 +12,11 @@ app.add_middleware(SessionMiddleware, secret_key=settings.JWT_SECRET)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:5173", "https://mind-flow-steel.vercel.app"],
+    allow_origins=[
+        "http://localhost:8080",
+        "http://localhost:5173",
+        "https://mindflow-vexy1.vercel.app",
+    ],
     allow_origin_regex=r"^chrome-extension://.*$",
     allow_credentials=True,
     allow_methods=["*"],
